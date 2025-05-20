1dce69a7cc4c4c2c79564a420458da54035f5c0a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-05-20 13:53:46 +0200

    Add tmt metadata

---
+++ tmt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+if [[ -z $FOREMAN_BASE_URL ]] ; then
+	if [[ -n $TMT_TOPOLOGY_BASH ]] ; then
+		. "$TMT_TOPOLOGY_BASH"
+		FOREMAN_BASE_URL=https://${TMT_GUESTS['quadlet.hostname']}/
+	else
+		echo "Provide $FOREMAN_BASE_URL" >&2
+		exit 1
+	fi
+fi
+
+
+pytest --base-url "${FOREMAN_BASE_URL}" "$@"
